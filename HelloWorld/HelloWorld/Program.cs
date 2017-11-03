f965df04fcf1f56be0dcb1007f9c42b5969a68cf
joe--public <jo.nineo@gmail.com>
2017-11-03 08:40:25 +0000

    Basic "hello world" console application. Test connection to git from visual studio 2015.

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Format("Program Name: {0}{1}", ProgramName, Environment.NewLine));
+
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+
+            Console.Write("Press any key to exit.");
+            Console.Read();
+        }
+
+        static string ProgramName
+        {
+            get
+            {
+                string[] segments = Assembly
+                    .GetEntryAssembly()
+                    .GetName()
+                    .ToString()
+                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                return segments[0];
+            }
+        }
+    }
+}
